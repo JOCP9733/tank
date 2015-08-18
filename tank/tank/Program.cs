5c3eb6c5a8afed82506b90a58238b32db7071051
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-08-18 14:10:34 +0200

    added Otter

---
+++ tank/tank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
