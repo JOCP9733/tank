448d1263153729c26f61f3459dd84bd359ebd7d2
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-08-18 14:27:50 +0200

    added folder structure

---
+++ tank/tank/Code/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tank.Code
+{
+    /// <summary>
+    /// This class should include practical methods, which are often needed.
+    /// </summary>
+    class Utilities
+    {
+
+
+
+    }
+}
