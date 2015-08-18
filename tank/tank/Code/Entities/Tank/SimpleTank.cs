7a14e617d1d23e4ffdf909fac0947922120b8988
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-08-18 14:54:11 +0200

    managed Tank classes

--- tank/tank/Code/Entities/Tank/SimpleTank.cs
+++ tank/tank/Code/Entities/Tank/SimpleTank.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace tank.Code.Entities.Tank
 {
-    class SimpleTank
+    /// <summary>
+    /// Simple Tank, which should be initialized as a Tank with needed
+    /// decorators
+    /// </summary>
+    class SimpleTank : Tank
     {
+        /// <summary>
+        /// Methode which is necessary to implement simple driving
+        /// </summary>
+        public override void drive()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        ///  Methode which is necessary to implement simple shooting
+        /// </summary>
+        public override void shoot(float x , float y)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- tank/tank/Code/Entities/Tank/Tank.cs
+++ tank/tank/Code/Entities/Tank/Tank.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Otter;
+
 
 namespace tank.Code.Entities.Tank
 {
-    class Tank
+    /// <summary>
+    /// Static Type for the Tank class
+    /// </summary>
+    abstract class Tank : Entity
     {
+        /// <summary>
+        /// abstract Method, which should be ovrwritten by 
+        /// the Decorators
+        /// </summary>
+        abstract public void shoot(float x, float y);
+        /// <summary>
+        /// abstract Method, which should be ovrwritten by 
+        /// the Decorators
+        /// </summary>
+        abstract public void drive();
+
     }
 }
