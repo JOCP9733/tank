6781791de74adaea30c434b832eb43015be07062
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-08-19 23:55:01 +0200

    added missing class

---
+++ tank/tank/Code/Entities/Tank/Logics/Decorators/ControlSimpleKI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tank.Code.Entities.Tank.Logics.Decorators
+{
+    class ControlSimpleKI : LogicDecorator
+    {
+        public ControlSimpleKI(ILogic pLogic) : base(pLogic)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            move();
+        }
+
+        private void move()
+        {
+            Tank.move_forward();
+            Tank.move_turn_right();
+
+        }
+
+
+    }
+}
