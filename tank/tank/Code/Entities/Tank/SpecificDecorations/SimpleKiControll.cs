9525e4e78e73ddf36c59847b893124ebe1993e9d
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-08-19 21:12:45 +0200

    startedt first Ki

---
+++ tank/tank/Code/Entities/Tank/SpecificDecorations/SimpleKiControll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tank.Code.Entities.Tank.SpecificDecorations
+{
+    class SimpleKiControll :TankDecorator
+    {
+
+        private int steps;
+        public SimpleKiControll(Tank t) : base(t)
+        {
+            tank = t;
+        }
+
+        public override void drive()
+        {
+            
+        }
+
+    }
+}
